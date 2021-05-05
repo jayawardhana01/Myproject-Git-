c0c3f525d11959a49a16a417b62c8395b8ac24a3
jayawardhana01 <72667542+jayawardhana01@users.noreply.github.com>
2021-05-05 12:59:59 +0500

    update file

---
+++ python code.py
@@ -0,0 +1,8 @@
+def remove_duplicates(lista):
+         lista2[]
+         for item in lista:
+             if item not in lista2:#is item in lista2 alredy?
+                lista2.uppend(item)
+             return lista(item)
+          return lista(item)
+print (remove_dupplicates([1,2,3]))
